26ddc45759d6b88c2e69252f3ae9dacd4f68efeb
malek <malek.2006ar@gmail.com>
2026-09-08 00:40:03 +0300

    Add User model

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from app.database import Base
+
+
+class User(Base):
+    """User model."""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
